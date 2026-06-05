abdcbd3eedad081a3cf0b225ec4a452ae69f2b9d
Muhammad Naufal Farras <opal.farras@gmail.com>
2026-06-05 14:54:39 +0700

    feat(talent-growth): Setup express server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,42 @@
-import express from "express";
+import express, { Application } from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
 
-const app = express();
+import authRoutes from "./routes/authRoutes.ts";
+import postRoutes from "./routes/postRoutes.ts";
+import commentRoutes from "./routes/commentRoutes.ts";
 
-app.get("/", (_req, res) => {
-  res.send("Hello Express!");
-});
+dotenv.config();
+
+const app: Application = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/auth", authRoutes);
+app.use("/posts", postRoutes);
+app.use("/comments", commentRoutes);
+
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI as string;
+
+if (!MONGO_URI) {
+  console.error("FATAL ERROR: MONGO_URI is not defined in .env file");
+  process.exit(1);
+}
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log("Successfull connect with MongoDB");
+
+    app.listen(PORT, () => {
+      console.log(`Server run on http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Failed to connect with MongoDB: ", error);
+  });
 
 export default app;
