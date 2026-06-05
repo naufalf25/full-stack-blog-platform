59da9e2c1340fa1f1cb591b66b5aab8778178fac
Muhammad Naufal Farras <opal.farras@gmail.com>
2026-06-06 06:24:40 +0700

    feat(talent-growth): Configure express for deploy to vercel, separate prod and dev file

---
+++ backend/src/index.ts
@@ -0,0 +1,38 @@
+import express from "express";
+import type { Application } from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/authRoutes.ts";
+import postRoutes from "./routes/postRoutes.ts";
+import commentRoutes from "./routes/commentRoutes.ts";
+
+dotenv.config();
+
+const app: Application = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/comments", commentRoutes);
+
+const MONGO_URI = process.env.MONGO_URI as string;
+
+if (!MONGO_URI) {
+  console.error("FATAL ERROR: MONGO_URI is not defined in .env file");
+  process.exit(1);
+}
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log("Successfull connect with MongoDB");
+  })
+  .catch((error) => {
+    console.error("Failed to connect with MongoDB: ", error);
+  });
+
+export default app;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,43 +1,6 @@
-import express from "express";
-import type { Application } from "express";
-import mongoose from "mongoose";
-import cors from "cors";
-import dotenv from "dotenv";
-
-import authRoutes from "./routes/authRoutes.ts";
-import postRoutes from "./routes/postRoutes.ts";
-import commentRoutes from "./routes/commentRoutes.ts";
-
-dotenv.config();
-
-const app: Application = express();
-
-app.use(cors());
-app.use(express.json());
-
-app.use("/api/auth", authRoutes);
-app.use("/api/posts", postRoutes);
-app.use("/api/comments", commentRoutes);
+import app from "./index.ts";
 
 const PORT = process.env.PORT || 5000;
-const MONGO_URI = process.env.MONGO_URI as string;
-
-if (!MONGO_URI) {
-  console.error("FATAL ERROR: MONGO_URI is not defined in .env file");
-  process.exit(1);
-}
-
-mongoose
-  .connect(MONGO_URI)
-  .then(() => {
-    console.log("Successfull connect with MongoDB");
-
-    app.listen(PORT, () => {
-      console.log(`Server run on http://localhost:${PORT}`);
-    });
-  })
-  .catch((error) => {
-    console.error("Failed to connect with MongoDB: ", error);
-  });
-
-export default app;
+app.listen(PORT, () => {
+  console.log(`Server run on http://localhost:${PORT}`);
+});
